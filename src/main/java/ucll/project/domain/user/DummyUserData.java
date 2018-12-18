242d17e84b9e354d3f8bad1daa1800451a532526
bobbobboe <bvds2152@gmail.com>
2018-12-18 11:36:25 +0100

    removed bob

--- src/main/java/ucll/project/domain/user/DummyUserData.java
+++ src/main/java/ucll/project/domain/user/DummyUserData.java
@@ -11,16 +11,6 @@ public class DummyUserData {
                 "admin" // password
         );
 
-        userRepository.createUser(
-                new User(
-                        "bob",
-                        "Bob",
-                        "Van der Smissen",
-                        "bob@iswleuven.be",
-                        Gender.MALE, Role.ADMIN
-                ),
-                "bob"
-        );
 
         userRepository.createUser(
                 new User("support",
