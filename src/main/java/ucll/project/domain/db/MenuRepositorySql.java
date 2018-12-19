025d1f2c96bc0ff4a47ac45794953b6b11337c41
Kwinnieprince <kwinten@delrue.com>
2018-12-19 11:13:14 +0100

    sql menu repo

--- src/main/java/ucll/project/domain/db/MenuRepositorySql.java
+++ src/main/java/ucll/project/domain/db/MenuRepositorySql.java
@@ -1,4 +1,11 @@
 package ucll.project.domain.db;
 
+import java.util.Properties;
+
 public class MenuRepositorySql {
+    private Properties properties;
+
+    public MenuRepositorySql(Properties properties){
+         this.properties = properties;
+    }
 }
